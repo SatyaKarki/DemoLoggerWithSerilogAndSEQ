edeb6f8cd7d0f2966bda551dd66902f09c33e95f
Satya Karki <spacegrlsatya@gmail.com>
2025-01-01 00:00:56 +0545

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+//// Configure Serilog from appsettings.json
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.UseSerilogRequestLogging();
+
+app.Run();
